d93752b937b5fbf7d90d67e71cf167717306d7cb
Sabarnika <98590604+Sabarnika@users.noreply.github.com>
2024-02-05 21:09:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ const expenseRouter = require("./expenseRouter");
 const userRouter = require("./userRouter");
 const app = express();
 dotenv.config();
-app.use(cors());
+app.use(cors({
+  origin: 'https://expense-tracker-two-alpha.vercel.app/user/sign-in','https://expense-tracker-two-alpha.vercel.app/user/sign-up,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+}));
+
 app.use(express.json());
 mongoose
   .connect(process.env.MONGO_URL)
